52cd6482ea571e02fba0d3555cc2fe63d22d5310
Maxime Le Duc <maximeleduc@unknown.home>
2024-01-24 12:19:06 +0100

    Add Tailwindcss

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,11 +1,12 @@
-import { Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-red-500">Home</Text>
       <StatusBar style="auto" />
-      <Text>Home Screen</Text>
     </View>
   );
 }
