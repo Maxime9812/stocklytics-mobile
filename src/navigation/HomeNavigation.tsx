74126c1a0596dcef031dfb65e3a9192ad0696b96
Maxime Le Duc <maximeleduc@unknown.home>
2024-01-25 18:53:00 +0100

    Icons tab bar

--- src/navigation/HomeNavigation.tsx
+++ src/navigation/HomeNavigation.tsx
@@ -12,6 +12,7 @@ import ItemsScreen from '../screens/home/items/ItemsScreen';
 import SearchScreen from '../screens/home/Search/SearchScreen';
 import NotificationScreen from '../screens/home/notification/NotificationScreen';
 import MenuScreen from '../screens/home/menu/MenuScreen';
+import { MaterialCommunityIcons, Feather, Ionicons } from '@expo/vector-icons';
 
 export type HomeTabParamList = {
   Dashboard: undefined;
@@ -31,17 +32,71 @@ export type HomeTabNavigator = NavigatorScreenParams<HomeTabParamList>;
 
 const Tab = createBottomTabNavigator<HomeTabParamList>();
 
+type IconProps = {
+  focused: boolean;
+  color: string;
+  size: number;
+};
+
+const DashboardIcon = (props: IconProps) => {
+  return <MaterialCommunityIcons name="view-dashboard-outline" {...props} />;
+};
+
+const ItemsIcon = (props: IconProps) => {
+  return <Feather name="archive" {...props} />;
+};
+
+const SearchIcon = (props: IconProps) => {
+  return <Feather name="search" {...props} />;
+};
+
+const NotificationIcon = (props: IconProps) => {
+  return <Ionicons name="notifications-outline" {...props} />;
+};
+
+const MenuIcon = (props: IconProps) => {
+  return <Feather name="menu" {...props} />;
+};
+
 export default function HomeNavigation() {
   return (
     <Tab.Navigator
       initialRouteName="Dashboard"
-      screenOptions={{ headerShown: false }}
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#f87171',
+      }}
     >
-      <Tab.Screen name="Dashboard" component={DashboardScreen} />
-      <Tab.Screen name="Items" component={ItemsScreen} />
-      <Tab.Screen name="Search" component={SearchScreen} />
-      <Tab.Screen name="Notifications" component={NotificationScreen} />
-      <Tab.Screen name="Menu" component={MenuScreen} />
+      <Tab.Screen
+        name="Dashboard"
+        component={DashboardScreen}
+        options={{
+          tabBarIcon: DashboardIcon,
+          tabBarBadgeStyle: {
+            backgroundColor: '#f87171',
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Items"
+        component={ItemsScreen}
+        options={{ tabBarIcon: ItemsIcon }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{ tabBarIcon: SearchIcon }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={NotificationScreen}
+        options={{ tabBarIcon: NotificationIcon }}
+      />
+      <Tab.Screen
+        name="Menu"
+        component={MenuScreen}
+        options={{ tabBarIcon: MenuIcon }}
+      />
     </Tab.Navigator>
   );
 }
