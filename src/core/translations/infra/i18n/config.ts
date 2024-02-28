f5459373f5dd987f1860a44e6109a3c352bcf6fd
Maxime Le Duc <maximeleduc@unknown.home>
2024-02-28 13:53:50 +0100

    Typesafe translation

--- src/core/translations/infra/i18n/config.ts
+++ src/core/translations/infra/i18n/config.ts
@@ -1,27 +1,22 @@
 import i18next from 'i18next';
 import 'intl-pluralrules';
 import { initReactI18next } from 'react-i18next';
+import translationEn from './en';
+import translationFr from './fr';
 
 import { TranslationGateway } from '../../hexagon/gateways/translation.gateway';
+
+export const defaultNS = 'common';
+export const resources = {
+  en: translationEn,
+  fr: translationFr,
+};
 
 const init = async (translationGateway: TranslationGateway) => {
   await i18next.use(initReactI18next).init({
     lng: translationGateway.getLocale(),
-    defaultNS: 'common',
-    resources: {
-      en: {
-        common: {},
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
-        auth: (await import('./en/auth')).default,
-      },
-      fr: {
-        common: {},
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
-        auth: (await import('./fr/auth')).default,
-      },
-    },
+    defaultNS,
+    resources,
   });
 };
 
